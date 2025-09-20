5541c9e73d24b403fb6b5d2f9355e5f9a5ee602b
Sébastien Randriamanana <sebastienrandriamanana3@gmail.com>
2025-09-20 21:24:29 +0400

    last update

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const hamburger = document.getElementById('hamburger');
-const navBar = document.querySelector('#nav-bar')
+const navBar = document.querySelector('#nav-bar');
+const cards =  document.querySelectorAll('#features .feature-card');
+const pricingCards = document.querySelectorAll('#pricing .pricing-card');
 
 hamburger.addEventListener('click', () => {
     navBar.classList.toggle('active');
 });
+
+const observer = new IntersectionObserver((entries)=>{
+   entries.forEach(entry=>{
+     if(entry.isIntersecting){
+        entry.target.classList.add("show");
+     }else{
+        entry.target.classList.remove("show");
+     }
+   })
+},{
+    rootMargin: "0px 0px -400px 400px"
+});
+
+cards.forEach( card=> observer.observe(card))
+pricingCards.forEach(pricingCard=> observer.observe(pricingCard));
+
